e9135aadfb234ca59a3939f9d7eff996f92bd7fe
Highlander0815 <o.mueller@outlook.at>
2024-03-07 19:01:17 +0100

    inital commit

---
+++ Characters/player_cat.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 80
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _ready():
+	update_animation_parameters(starting_direction)
+
+func _physics_process(_delta):
+	# Get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	update_animation_parameters(input_direction)
+	
+	#print(input_direction)
+	
+	# Update velocity
+	velocity = input_direction * move_speed
+	
+	# Move and Slide function uses velocity of character body to move character on map
+	move_and_slide()
+	
+	#print(velocity)
+	
+	pick_new_state()
+	
+func update_animation_parameters(move_input : Vector2):
+	# Dont change animation parameters if there is no move input
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+func pick_new_state():
+	# Choose state based on what is happening with the player
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
